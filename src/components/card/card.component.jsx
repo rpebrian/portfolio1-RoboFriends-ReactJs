2bb4b9ce4dd862ea6f2b59ea6904d871b15dcca5
rpeb <rizpebrian16@gmail.com>
2020-05-18 18:12:53 +0700

    deploy new

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -8,8 +8,8 @@ export const Card = (props) => {
     return(
     <div className='card-container'>
         <img src={`https://robohash.org/${props.biodata.id}?set=set2&size=180x180`} alt="biodata"/>
-        <h5>{props.biodata.employee_name}</h5>
-        <p>{props.biodata.employee_salary}</p>
+        <h5>{props.biodata.name}</h5>
+        <p>{props.biodata.email}</p>
     </div>
 )
 }
